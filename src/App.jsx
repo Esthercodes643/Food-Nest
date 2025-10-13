16d5c83deaecba53ee15da5d49d80b79ac915f57
Esther <Esther D>
2025-10-13 14:10:21 +0530

    Completed AppStore , Banner , testimonial Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Services from './components/Services/Services'
-
+import Banner from './components/Banner/Banner'
+import AppStore from './components/AppStore/AppStore'
+import Testimonial from './components/Testimonial/Testimonial'
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero /> 
-      <Services />       
+      <Services />  
+      <Banner />     
+      <AppStore />
+      <Testimonial />
     </div>
   )
 }
